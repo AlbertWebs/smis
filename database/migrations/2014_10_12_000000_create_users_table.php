54095a7ff965d0db188f0245ac604f4e718cd285
Albert Muhatia <albertmuhatia@gmail.com>
2022-11-04 17:38:52 +0300

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('organization_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_admin')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
